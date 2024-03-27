ad85b126269f5a9febf436ddb171d15cc2962ed1
Luca de Alfaro <luca@dealfaro.org>
2024-03-27 13:50:41 -0700

    Initial commit

---
+++ download_submissions.py
@@ -0,0 +1,118 @@
+# Copyright Luca de Alfaro, 2019
+# BSD License
+
+import argparse
+import csv
+from collections import defaultdict
+import datetime
+import os
+import pickle
+import random
+import zipfile
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly',
+            'https://www.googleapis.com/auth/drive.readonly']
+# https://www.googleapis.com/auth/spreadsheets for r/w
+#
+
+
+def main(args):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    sheet_service = build('sheets', 'v4', credentials=creds)
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    # Call the Sheets API
+    range_name = args.sheet + '!' + args.email_column + '2:' + args.file_column
+    sheet = sheet_service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=args.spreadsheet_id,
+                                range=range_name).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+
+    # Now downloads all the submissions.
+    student_submissions = {row[0]: row[-1] for row in values}
+    for email, url in student_submissions.items():
+        if args.test:
+            print(email, url)
+            continue
+        docid = url.split("=")[-1]
+        download_fn = os.path.join(args.destination_dir, email + '.' + args.extension)
+        # Creates the directory if necessary.
+        if not os.path.exists(args.destination_dir):
+            os.makedirs(args.destination_dir)
+        with open(download_fn, 'wb') as f:
+            request = drive_service.files().get_media(fileId=docid)
+            downloader = MediaIoBaseDownload(f, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print("Download %s to %s : %d%%." % (email, download_fn, int(status.progress() * 100)))
+        if not args.no_unzip: 
+            if args.extension == 'zip':
+                with zipfile.ZipFile(download_fn, 'r') as zip_ref:
+                    extraction_dir = os.path.join(args.destination_dir, email)
+                    zip_ref.extractall(extraction_dir)
+    # Finally, writes the csv file with all the students who have submitted,
+    # so their work can be included.
+    csv_fn = os.path.join(args.destination_dir, 'students.csv')
+    with open(csv_fn, 'w', newline='') as csvfile:
+        fieldnames=['email']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for email in student_submissions.keys():
+            writer.writerow({'email': email})
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--spreadsheet_id', default=None,
+                        help="ID of the spreadsheet to use as base of download.")
+    parser.add_argument('-m', '--email_column', default='B',
+                        help='Column containing email')
+    parser.add_argument('-f', '--file_column', default='C',
+                        help='Column containing submission')
+    parser.add_argument('--sheet', type=str, default='Form Responses 1',
+                        help='Sheet name')
+    parser.add_argument('-e', '--extension', type=str, default='zip',
+                        help='Extension to be used in download.'
+                        )
+    parser.add_argument('-z', '--no_unzip', action='store_true', default=False,
+                        help='Do not unzip the downloaded files.')
+    parser.add_argument('-c', '--destination_dir', type=str,
+                        default='.', help='Directory where to store the submissions.')
+    parser.add_argument('-t', '--test', action='store_true', default=False,
+                        help="Test but do not download anything")
+    args = parser.parse_args()
+    main(args)
