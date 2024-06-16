df1f351e2a1229611358ff889aad03d1f6ef5c42
Luca de Alfaro <luca@dealfaro.org>
2024-06-16 22:48:42 +0200

    First version of feedback

---
+++ file_distributor.py
@@ -0,0 +1,160 @@
+# Copyright Luca de Alfaro, 2019.
+# BSD License.
+
+import io
+import os
+import pickle
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload, MediaIoBaseUpload
+
+from file_distributor import FileDistributor
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+
+class FileDistributor(object):
+
+    def __init__(self, drive_service, share_folder_id):
+        """Initializes a file distributor."""
+        self.drive_service = drive_service
+        self.share_folder_id = share_folder_id
+
+    def _distribute_media(self, email, media, file_name, mode='reader', update=False):
+        """Distributes media to the student.
+        @:param email: email of the user.
+        @:param media: Google media object for upload.
+        @:param file_name: file name to use in drive.
+        @:param mode: mode with which to distribute the file.  It
+        can be one of 'reader', 'writer', 'commenter'.
+        @:param update: if True, check if a file by the same name exists,
+        and if so, updates the feedback.
+        """
+        assert mode in ['reader', 'writer', 'commenter']
+        # Uploads the file.
+        file_meta = {'name': file_name, 'parents': self.share_folder_id}
+        # do_create keeps track of whether we need to create the file.
+        do_create = not update
+        if update:
+            # Checks if a file by the same name exists.
+            existing_file_ids = []
+            q = "'%s' in parents" % self.share_folder_id
+            q += " and name = '%s'" % file_name
+            page_token = None
+            while True:
+                response = self.drive_service.files().list(
+                    q=q,
+                    spaces='drive',
+                    fields='nextPageToken, files(id, name)',
+                    pageToken=page_token).execute()
+                for file in response.get('files', []):
+                    existing_file_ids.append(file.get('id'))
+                page_token = response.get('nextPageToken', None)
+                if page_token is None:
+                    break
+            if len(existing_file_ids) == 0:
+                # We need to create.
+                do_create = True
+            else:
+                # We can perform the update.
+                # We update all files we find, because we could have old copies.
+                for file_id in existing_file_ids:
+                    self.drive_service.files().update(
+                        fileId=file_id,
+                        media_body=media
+                    ).execute()
+        if do_create:
+            # The new file is created.
+            upfile = self.drive_service.files().create(
+                body=file_meta,
+                media_body=media,
+                fields='id').execute()
+            file_id = upfile.get('id')
+            # Shares the file
+            user_permission = {
+                'type': 'user',
+                'role': mode,
+                'emailAddress': email
+            }
+            self.drive_service.permissions().create(
+                fileId=file_id,
+                body=user_permission,
+                fields='id',
+                sendNotificationEmail=False, # otherwise, Google throttles us
+            ).execute()
+
+
+    def distribute_file(self, email, file_path, file_name, mime,
+                        mode='reader', update=True):
+        """Distributes a file to a user.
+        @:param email: email of the user.
+        @:param file_path: path to file to distribute
+        @:param file_name: file name to use in drive.
+        @:param mime: mime type of file to be distributed
+        @:param mode: mode with which to distribute the file.  It
+        can be one of 'reader', 'writer', 'commenter'.
+        @:param update: if True, check if a file by the same name exists,
+        and if so, updates the feedback.
+        See https://developers.google.com/drive/api/v3/reference/permissions/create
+        """
+        media = MediaFileUpload(file_path, mimetype=mime, resumable=True)
+        self._distribute_media(email, media, file_name, mode, update=update)
+        print("Distributed %s to %s" % (file_name, email))
+
+
+    def distribute_bytes(self, email, content_bytes, file_name,
+                         mime='text/html', mode='reader', update=True):
+        """Distributes bytes to the user.
+        @:param email: email of the user.
+        @:param text: the text to distribute
+        @:param file_name: file name to use in drive.
+        @:param mime: mime type of file to be distributed.
+        Can also be 'text/html', etc.
+        @:param mode: mode with which to distribute the file.  It
+        can be one of 'reader', 'writer', 'commenter'.
+        @:param update: if True, check if a file by the same name exists,
+        and if so, updates the feedback.
+        """
+        buffer = io.BytesIO()
+        buffer.write(content_bytes)
+        media = MediaIoBaseUpload(buffer, mime, resumable=True)
+        self._distribute_media(email, media, file_name, mode, update=update)
+        buffer.close()
+        print("Distributed %s to %s" % (file_name, email))
+
+
+# The following is for testing.
+if __name__ == '__main__':
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    fd = FileDistributor(drive_service, 'my_class', 'some_id')
+    fd.distribute_bytes('luca@ucsc.edu',
+                        'Ciao sono io. \n\n Luca'.encode('utf-8'),
+                        'be_happy.txt', 'text/plain')
---
+++ upload_feedback.py
@@ -0,0 +1,120 @@
+# Copyright Luca de Alfaro, 2019
+# BSD License
+
+import argparse
+import csv
+import os
+import pickle
+import shutil
+import time
+import yatl
+import zipfile
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+
+from file_distributor import FileDistributor
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly',
+            'https://www.googleapis.com/auth/drive']
+# https://www.googleapis.com/auth/spreadsheets for r/w
+
+
+def main(args):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server()
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    sheet_service = build('sheets', 'v4', credentials=creds)
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    # Call the Sheets API
+    range_name = args.sheet
+    sheet = sheet_service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=args.spreadsheet_id,
+                                range=range_name).execute()
+    values = result.get('values', [])
+
+    if not values or len(values) == 1:
+        print('No data found.')
+        return
+    
+    # Creates the file distributor.
+    file_distributor = FileDistributor(drive_service, args.destination_dir)
+    
+    headers = values[0]
+    # Computes reverse index of header to column.
+    header2column = {header: i for i, header in enumerate(headers)}
+    # Figures out which column is the email. 
+    if args.email is not None:
+        email_column = header2column[args.email]
+    else:
+        possible_email_columns = [header2column[h] for h in headers if 'email' in h.lower()]
+        if len(possible_email_columns) == 1:
+            email_column = possible_email_columns[0]
+        else:
+            print(f"Could not determine email column from headers {headers}")
+            return
+        
+    # Produces the text. 
+    for row in values[1:]:
+        email = values[email_column]
+        # We only process the required students. 
+        if args.students is not None and email not in args.students:
+            continue
+        d = {headers[i]: row[i] for i in range(len(row))}
+        with open(args.feedback, 'r') as f:
+            text = yatl.render(f.read(), d)
+        if args.test:
+            print("Feedback for", email, ":")
+            print(text)
+            break
+        # Shares the text. 
+        filename = f'feedback_{email}.html'
+        # file_distributor.distribute_bytes(
+        #     email, text.encode('utf-8'), filename, mime='text/plain', mode='reader', update=True)
+        print("Distributed feedback to", email)
+    print("Done.")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--spreadsheet_id', default=None,
+                        help="ID of the spreadsheet to use as base of download.")
+    parser.add_argument('--sheet', type=str, default='Form Responses 1',
+                        help='Sheet name')
+    parser.add_argument('-f', '--feedback', type=str, default=None,
+                        help="Feedback yatl template")
+    parser.add_argument('-m', '--email_column', default=None,
+                        help='Column containing email, if title is not email')
+    parser.add_argument('-d', '--destination_dir', type=str, default=None, 
+                        help='Destination folder ID in Drive for the feedback')
+    parser.add_argument('-t', '--test', action='store_true', default=False,
+                        help="Test but do not share anything")
+    parser.add_argument('--students', type=str, default=None,
+                        help='Comma-separated list of student emails whose feedback we want to share')
+    args = parser.parse_args()
+    main(args)
